7450f7b8ca13d9524a77a10837d9286850509b35
flores-97 <floresvictor15@gmail.com>
2025-10-22 19:00:47 -0700

    corrected issues. mistakenly added"long" instead of log

--- script1.js
+++ script1.js
@@ -1,44 +1,37 @@
-alert("Hello World!");
-// this is a single line comment.
-/*
-this
-is
-a 
-multi
-line
-comment
-*/ 
-
 // Values, Types, and Operators
 console.log("Values, Types, and Operators");
 
-console.log("********************************************************")
+console.log("********************************************************");
 console.log("Boolean");
 //Boolean
-console.long(true); //example of boolean with true value
+console.log(true); //example of boolean with true value
 console.log(false); //example of boolean with false value
 
-console.log("********************************************************")
+console.log("********************************************************");
 console.log("Nulls");
 //null
 console.log(null);
 
-console.log("********************************************************")
+console.log("********************************************************");
 console.log("Undefined");
 //undefined
 let myNumber;
 console.log(myNumber);
 
-console.log("********************************************************")
+console.log("********************************************************");
 console.log("Numbers");
 //number
 console.log(15); //example of a whole number
 console.log(2.22); //example of floating-point number 
 console.log(5e5); //example of an exponent
 
-console.log("********************************************************")
+console.log("********************************************************");
 console.log("Strings");
 //string
-console.log("Hello world!") //example with double quotes
-console.log('Hello world!') //example with single quotes
-console.log(`Hello world!`) //example with back ticks
+console.log("Hello world!"); //example with double quotes
+console.log('Hello world!'); //example with single quotes
+console.log(`Hello world!`); //example with back ticks
+
+console.log("********************************************************");
+//special characters when manipulating strings
+console.log
