8eed16392fb5b735317001d884300f3620014f93
flores-97 <floresvictor15@gmail.com>
2025-10-22 19:10:38 -0700

    added operators to js file

--- script1.js
+++ script1.js
@@ -34,4 +34,43 @@ console.log(`Hello world!`); //example with back ticks
 
 console.log("********************************************************");
 //special characters when manipulating strings
-console.log
+console.log("Special characters when manipulating strings");
+console.log("\nHello"); //creates new line
+console.log("\tHello"); //creates a tab
+
+console.log("********************************************************");
+//operators
+console.log("********************************************************");
+
+
+console.log("********************************************************");
+//add
+console.log("Add");
+console.log(5+2); //add whole numbers
+console.log(10.5+8.754); //add floating points
+console.log("Hello" + "world" + "!"); //concatenating string
+
+
+console.log("********************************************************");
+//subtract
+console.log("Subtracting");
+console.log(100-25); //subtraction
+
+
+console.log("********************************************************");
+//multiply
+console.log("Multiplication");
+console.log(91*79); //multiplication
+
+
+console.log("********************************************************");
+//division
+console.log("Division");
+console.log(12/5); //division
+
+
+console.log("********************************************************");
+//modulo
+console.log("Modulo");
+console.log(12 % 5); //modulo
+
